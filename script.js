e2c0bf4fe5d527f5fcc0ce4508ee5b8386c3f441
dpurcell <dpurcell.pd@gmail.com>
2023-06-29 17:42:53 -0400

    live greeting functionality

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+class TextGenerator {
+    constructor(name) {
+        this.name = name;
+    }
+
+    static greeting(name) {
+        const TEXT_SECTION = document.querySelector("#text-section");
+        let GAME_TEXT = document.createElement("p");
+        GAME_TEXT.innerHTML = 
+        `A pleasure to make your acquaintance, ${name}.
+        I understand you're here to try your hand at my game.
+        <br><br>`;
+        TEXT_SECTION.appendChild(GAME_TEXT);
+        
+        setTimeout(() => {
+            GAME_TEXT.innerHTML += "Allow me to set the stage...";            
+            TEXT_SECTION.appendChild(GAME_TEXT);
+        }, 3000);
+        
+    }
+}
+
 class Hangman {
     static Game() {
         let missed;
         let guesses;
         let wordArray = ["apples", "peaches", "pears", "grapes", "oranges", "nectarines", "plums", "dragonfruit", "coconut", "kiwi"];
         let repeat = "y";
+        let flag = true;
 
-        console.log('you are running the game');
-
-        const TEXT_SECTION = document.querySelector("#text-section");
-        const GAME_TEXT = document.createElement("p");
-        const INTRO = prompt("Hello, what is your name?");
+        const NAME = prompt("Hello, what is your name?");
+        TextGenerator.greeting(NAME);       
        
+        
+        
+        
     }
 }
 
