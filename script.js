2e0a89831ec2292b03ad0aa231261d996f682099
dpurcell <dpurcell.pd@gmail.com>
2023-06-30 19:12:44 -0400

    random answer functionality

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-class TextGenerator {
-    constructor(name) {
-        this.name = name;
+class Hangman {
+
+    static randomNumber(arrayLength) {
+        const index = arrayLength - 1;
+        return Math.floor(Math.random() * index);
     }
 
-    static greeting(name) {
-        const TEXT_SECTION = document.querySelector("#text-section");
-        let GAME_TEXT = document.createElement("p");
-        GAME_TEXT.innerHTML = 
-        `A pleasure to make your acquaintance, ${name}.
+    static intro() {
+        const WORD_ARRAY = [
+            "apples", "peaches", "pears", "grapes", "oranges", 
+            "nectarines", "plums", "dragonfruit", "coconut", "kiwi"
+        ];
+        const LENGTH = WORD_ARRAY.length;
+        const ANSWER = WORD_ARRAY[Hangman.randomNumber(LENGTH)];           
+        const INTRO_SECTION = document.querySelector("#intro-section");
+        const NAME = prompt("Hello, what is your name?");         
+        let introText = document.createElement("p");
+        introText.innerHTML = 
+        `A pleasure to make your acquaintance, ${NAME}.
         I understand you're here to try your hand at my game.
         <br><br>`;
-        TEXT_SECTION.appendChild(GAME_TEXT);
+        INTRO_SECTION.appendChild(introText);
         
         setTimeout(() => {
-            GAME_TEXT.innerHTML += "Allow me to set the stage...";            
-            TEXT_SECTION.appendChild(GAME_TEXT);
-        }, 3000);
+            introText.innerHTML += "Allow me to set the stage...";            
+            INTRO_SECTION.appendChild(introText);
+        }, 2000);   
+        
+        NewRound.play(ANSWER);
+    }
+}
+
+class NewRound {
+    static play(word) {
         
     }
 }
 
-class Hangman {
-    static Game() {
-        let missed;
-        let guesses;
-        let wordArray = ["apples", "peaches", "pears", "grapes", "oranges", "nectarines", "plums", "dragonfruit", "coconut", "kiwi"];
-        let repeat = "y";
-        let flag = true;
-
-        const NAME = prompt("Hello, what is your name?");
-        TextGenerator.greeting(NAME);       
-       
-        
-        
-        
-    }
-}
-
-const GAME_BUTTON = document.getElementById("game");
-GAME_BUTTON.addEventListener("click", Hangman.Game);
+const GAME_BUTTON = document.getElementById("game-btn");
+GAME_BUTTON.addEventListener("click", Hangman.intro);
