e0d94cde765e02309e7a5de5b24fa935d5367471
dpurcell <dpurcell.pd@gmail.com>
2023-07-06 15:34:17 -0400

    reworked code, dynamic HTML, base NewRound functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-class Hangman {
+class Hangman { 
+
+    static guess;
+    static answer;
+    static wordLength;
 
     static introSection = document.querySelector("#intro-section");
     static introText = document.createElement("p");
@@ -7,17 +11,19 @@ class Hangman {
     static spaces = document.createElement("p");
     
     static gameTextSection = document.querySelector("#game-text-section");
-    static gameText = document.createElement("p");
-    
-    static rulesSection = document.querySelector("#rules-section");
-    static rulesText = document.createElement("p");
-
-    static chancesSection = document.querySelector("chances");
-    static chances = document.createElement("p");
+    static gameText = document.createElement("p");    
 
     static randomNumber(arrayLength) {
         const index = arrayLength - 1;
         return Math.floor(Math.random() * index);
+    }
+
+    static submitGuess() {           
+        const GUESS_INPUT = document.getElementById("guess");
+        Hangman.guess = GUESS_INPUT.value;
+        console.log(`Current guess: ${Hangman.guess}`);
+        GUESS_INPUT.value = "";  
+        NewRound.play(Hangman.answer, Hangman.wordLength);  
     }
 
     static intro() {
@@ -26,9 +32,9 @@ class Hangman {
             "nectarines", "plums", "dragonfruit", "coconut", "kiwi"
         ];
         const ARRAY_LENGTH = WORD_ARRAY.length;
-        const ANSWER = WORD_ARRAY[Hangman.randomNumber(ARRAY_LENGTH)];
-        const WORD_LENGTH = ANSWER.length;      
-        console.log(ANSWER);        
+        Hangman.answer = WORD_ARRAY[Hangman.randomNumber(ARRAY_LENGTH)];
+        Hangman.wordLength = Hangman.answer.length;      
+        console.log(Hangman.answer);        
         const NAME = prompt("Hello, what is your name?");        
        
         Hangman.introText.innerHTML = 
@@ -37,54 +43,54 @@ class Hangman {
         <br><br>
         
         Allow me to set the stage...
-        <br>`;  
+        <br><br>`;
+        
+        setTimeout(() => {
+            const GAME_SECTION = document.getElementById("game-section");
+            GAME_SECTION.innerHTML = `
+                <form id="form">
+                    <label for="guess"> Guess:
+                        <input placeholder="Enter Your Guess" type="text" id="guess" name="guess">
+                    </label>
+                    <br><br>
+                    <button type="button" id="submit-btn">Submit Guess</button>
+                </form>
+            `
+            for (let i = 0; i < Hangman.wordLength; i++) {
+                Hangman.spaces.innerHTML += "_ ";
+            }        
+            Hangman.spacesSection.appendChild(Hangman.spaces);
+
+            Hangman.introText.innerHTML += 
+            `You see before you spaces representing the word you must guess.
+            <br><br>
+            
+            As I'm sure you've also noticed, you will have a limited number of changes 
+            to guess the word in full.`;
+
+            const SUBMIT_BUTTON = document.getElementById("submit-btn");
+            SUBMIT_BUTTON.addEventListener("click", Hangman.submitGuess);
+        }, 4000);
         
         Hangman.introSection.appendChild(Hangman.introText);        
-        NewRound.play(ANSWER, WORD_LENGTH);
+        
     }
 }
 
-class NewRound {
-    static play(word, length) {
-        let spacesArray = Hangman.spaces.innerHTML.split("");
-        let guessArray = word.split("");        
-       
-        for (let i = 0; i < length; i++) {
-            Hangman.spaces.innerHTML += "_";
-        }            
-        
-        Hangman.spacesSection.appendChild(Hangman.spaces);
-
-        Hangman.rulesText.innerHTML += 
-        `You see before you spaces representing the word you must guess.
-        <br><br>
-        
-        As I'm sure you've also noticed, you will have a limited number of changes 
-        to guess the word in full.`;
-
-        Hangman.rulesSection.appendChild(Hangman.rulesText);
-
-        while (Hangman.spaces.innerHTML.includes("_")) {
-            guess = prompt("Guess a letter: ");
-
-            if (word.includes(guess)) {
-                for (let i = 0; i < word.length; i++) {
-                    if (word.split("")[i] == guess) {
-                        guess[i] = word[i];
-                    }
-                }
-            }
-            
-        };
-
-        if (word === guess ) {
-            console.log("Win detected!");
-        }
-    }
-
-    
+class NewRound {    
+    static play(word, wordLength) {
+        const GUESS = Hangman.guess;
+        const ANSWER = word;
+       if (String(ANSWER).includes(GUESS)) {
+        console.log("Matching letter found!");
+       }
+    }    
 }
-
 
 const GAME_BUTTON = document.getElementById("game-btn");
 GAME_BUTTON.addEventListener("click", Hangman.intro);
+
+
+
+
+
